e4b87919a54a47578072edd50c30b8f9f00eeb7f
axcell-0 <121089024+axcell-0@users.noreply.github.com>
2023-12-12 14:12:41 +0100

    Modify book.rb

--- book.rb
+++ book.rb
@@ -1,13 +1,22 @@
 class Book
-  attr_accessor :title, :author, :rentals
+  attr_accessor :title, :author, :rentals, :id
 
-  def initialize(title, author)
+  def initialize(title, author, id: Random.rand(1...1000))
     @title = title
     @author = author
     @rentals = []
+    @id = id
   end
 
   def add_rental(rental)
     rentals << rental
+  end
+
+  def to_json(_options = {})
+    {
+      title: @title,
+      author: @author,
+      'book.id': @id
+    }.to_json
   end
 end
