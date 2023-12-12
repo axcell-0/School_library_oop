8a4bbc05d1de8bf74dd261ba9b20f238995aa941
Samuel <samif3443@gmail.com>
2023-12-11 23:53:53 -0800

    Add teacher.rb file

---
+++ teacher.rb
@@ -0,0 +1,10 @@
+class Teacher < Person
+  attr_accessor :specialization
+  def initialize(age, name, specialization = 'Unknown')
+    super(age: age, name: name, parent_permission: true)
+    @specialization = specialization
+  end
+  def can_use_services?
+    true
+  end
+end
