27ef20a986f6654d30951bdfcd235d97f428151f
axcell-0 <121089024+axcell-0@users.noreply.github.com>
2023-12-12 10:52:30 +0100

    Fix linters

--- app.rb
+++ app.rb
@@ -36,7 +36,7 @@ class App
       { age: age, name: name, parent_permission: parent_permission }
     end
   end
-  
+
   def create_student
     student_details = StudentCreator.fetch_student_details
     person = Student.new(student_details[:name], student_details[:age], student_details[:parent_permission])
@@ -55,7 +55,7 @@ class App
       { age: age, name: name, specialization: specialization }
     end
   end
-  
+
   def create_teacher
     teacher_details = TeacherCreator.fetch_teacher_details
     person = Teacher.new(teacher_details[:age], teacher_details[:specialization], name: teacher_details[:name])
@@ -72,7 +72,7 @@ class App
       { title: title, author: author }
     end
   end
-  
+
   def create_book
     book_details = BookCreator.fetch_book_details
     book = Book.new(book_details[:title], book_details[:author])
@@ -89,7 +89,7 @@ class App
       book_index = gets.chomp.to_i
       books[book_index]
     end
-  
+
     def self.fetch_person_selection(people)
       puts 'Select a person from the following list by number (not id)'
       people.each_with_index do |person, index|
@@ -99,13 +99,13 @@ class App
       person_index = gets.chomp.to_i
       people[person_index]
     end
-  
+
     def self.fetch_date
       print 'Date: '
       gets.chomp
     end
   end
-  
+
   def create_rental
     book = RentalCreator.fetch_book_selection(@books)
     person = RentalCreator.fetch_person_selection(@people)
