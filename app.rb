dd3bed6d6ef15b8d8236fe88db42eb23ab1950b6
axcell-0 <121089024+axcell-0@users.noreply.github.com>
2023-12-12 10:46:21 +0100

    Modify App.rb

--- app.rb
+++ app.rb
@@ -25,55 +25,92 @@ class App
     end
   end
 
+  class StudentCreator
+    def self.fetch_student_details
+      print 'Age: '
+      age = gets.chomp.to_i
+      print 'Name: '
+      name = gets.chomp
+      print 'Has parent permission? [Y/N]: '
+      parent_permission = gets.chomp
+      { age: age, name: name, parent_permission: parent_permission }
+    end
+  end
+  
   def create_student
-    print 'Age: '
-    age = gets.chomp.to_i
-    print 'Name: '
-    name = gets.chomp
-    print 'Has parent permission? [Y/N]: '
-    parent_permission = gets.chomp
-    person = Student.new(name, age, parent_permission)
+    student_details = StudentCreator.fetch_student_details
+    person = Student.new(student_details[:name], student_details[:age], student_details[:parent_permission])
     @people.push(person)
-    puts "Student '#{name}' created successfully"
+    puts "Student '#{student_details[:name]}' created successfully"
   end
 
+  class TeacherCreator
+    def self.fetch_teacher_details
+      print 'Age: '
+      age = gets.chomp.to_i
+      print 'Name: '
+      name = gets.chomp
+      print 'Specialization: '
+      specialization = gets.chomp
+      { age: age, name: name, specialization: specialization }
+    end
+  end
+  
   def create_teacher
-    print 'Age: '
-    age = gets.chomp.to_i
-    print 'Name: '
-    name = gets.chomp
-    print 'Specialization: '
-    specialization = gets.chomp
-    person = Teacher.new(age, specialization, name: name)
+    teacher_details = TeacherCreator.fetch_teacher_details
+    person = Teacher.new(teacher_details[:age], teacher_details[:specialization], name: teacher_details[:name])
     @people.push(person)
-    puts "Teacher '#{name}' created successfully"
+    puts "Teacher '#{teacher_details[:name]}' created successfully"
   end
 
+  class BookCreator
+    def self.fetch_book_details
+      print 'Title: '
+      title = gets.chomp
+      print 'Author: '
+      author = gets.chomp
+      { title: title, author: author }
+    end
+  end
+  
   def create_book
-    print 'Title: '
-    title = gets.chomp
-    print 'Author: '
-    author = gets.chomp
-    book = Book.new(title, author)
+    book_details = BookCreator.fetch_book_details
+    book = Book.new(book_details[:title], book_details[:author])
     @books.push(book)
-    puts "Book '#{title}' created successfully"
+    puts "Book '#{book_details[:title]}' created successfully"
   end
 
+  class RentalCreator
+    def self.fetch_book_selection(books)
+      puts 'Select a book from the following list by number'
+      books.each_with_index do |book, index|
+        puts "#{index}) Title: \"#{book.title}\", Author: #{book.author}"
+      end
+      book_index = gets.chomp.to_i
+      books[book_index]
+    end
+  
+    def self.fetch_person_selection(people)
+      puts 'Select a person from the following list by number (not id)'
+      people.each_with_index do |person, index|
+        type = person.instance_of?(Student) ? 'Student' : 'Teacher'
+        puts "#{index}) [#{type}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
+      end
+      person_index = gets.chomp.to_i
+      people[person_index]
+    end
+  
+    def self.fetch_date
+      print 'Date: '
+      gets.chomp
+    end
+  end
+  
   def create_rental
-    puts 'Select a book from the following list by number'
-    @books.each_with_index do |book, index|
-      puts "#{index}) Title: \"#{book.title}\", Author: #{book.author}"
-    end
-    book_index = gets.chomp.to_i
-    puts 'Select a person from the following list by number (not id)'
-    @people.each_with_index do |person, index|
-      type = person.instance_of?(Student) ? 'Student' : 'Teacher'
-      puts "#{index}) [#{type}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
-    end
-    person_index = gets.chomp.to_i
-    print 'Date: '
-    date = gets.chomp
-    @rentals.push(Rental.new(date, @books[book_index], @people[person_index]))
+    book = RentalCreator.fetch_book_selection(@books)
+    person = RentalCreator.fetch_person_selection(@people)
+    date = RentalCreator.fetch_date
+    @rentals.push(Rental.new(date, book, person))
     puts 'Rental created successfully'
   end
 
